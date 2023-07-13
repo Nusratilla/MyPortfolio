dfe0099da714c1093b7dc94a28f471369dd7b1d9
Nusrat <nusratilla.88@gmail.com>
2023-07-13 16:41:12 +0500

    3

--- js/app.js
+++ js/app.js
@@ -1,16 +1,19 @@
-//--------------- Menu Bar ---------------
-const menuIcon = document.querySelector('#menu-icon');
-const navbar = document.querySelector('.navbar');
+window.addEventListener('DOMContentLoaded', () => {
+    //--------------- Menu Bar ---------------
+    const menuIcon = document.querySelector('#menu-icon');
+    const navbar = document.querySelector('.navbar');
 
-menuIcon.addEventListener('click', () => {
-    menuIcon.classList.toggle('bi-x');
-    navbar.classList.toggle('active');
-});
+    menuIcon.addEventListener('click', () => {
+        menuIcon.classList.toggle('bi-x');
+        navbar.classList.toggle('active');
+    });
 
-// ---------------Dark Theme--------------- 
-var iconMoon = document.querySelector('#icon-moon')
- 
-iconMoon.addEventListener('click', () => {
-    document.body.classList.toggle('dark-theme');
-    iconMoon.classList.toggle('bi-sun-fill');
+    // ---------------Dark Theme--------------- 
+    var iconMoon = document.querySelector('#icon-moon')
+    
+    iconMoon.addEventListener('click', () => {
+        document.body.classList.toggle('dark-theme');
+        iconMoon.classList.toggle('bi-sun-fill');
+    });
+
 });
