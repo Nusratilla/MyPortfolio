f0381752e03edbc1687c2e88f3fa017f33af9dfa
Nusrat <nusratilla.88@gmail.com>
2023-07-14 23:35:14 +0500

    local storage js

--- js/app.js
+++ js/app.js
@@ -9,16 +9,25 @@ window.addEventListener('DOMContentLoaded', () => {
     });
 
     // ---------------Dark Theme--------------- 
-    var iconMoon = document.querySelector('#icon-moon')
+    
+    const iconMoon = document.querySelector('#icon-moon')
+    const darkMode = localStorage.getItem('darkMode')
+
+    if (darkMode) document.body.classList.add(darkMode)
     
     iconMoon.addEventListener('click', () => {
         document.body.classList.toggle('dark-theme');
         iconMoon.classList.toggle('bi-sun-fill');
+        if (document.body.classList.contains('dark-theme')) {
+            localStorage.setItem('darkMode', 'dark-theme')
+        } else {
+            localStorage.removeItem('darkMode')
+        }
     });
 
     // ---------------typed animation--------------- 
     const typed = new Typed('.name-input', {
-        strings: ['Nusratilla Rikhsiev', 'Front-End Web Developer', 'UI UX Designer'],
+        strings: ['Nusratilla Rikhsiev', 'Front-End Web Developer'],
         typeSpeed: 90,
         backSpeed: 60,
         loop: true
