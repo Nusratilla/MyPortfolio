bd5fe0122141d9c4a22ee2aeff4ac85085354763
Nusrat <nusratilla.88@gmail.com>
2023-07-17 18:41:10 +0500

    br

--- data/skills.js
+++ data/skills.js
@@ -1,10 +1,46 @@
 const skillsDb = [
   {
     image: 'HTML5.png',
-    sname: 'HTML'
+    sname: 'HTML5'
   },
   {
-    image: 'HTML5.png',
-    sname: 'HTML'
-  }
+    image: 'CSS3.png',
+    sname: 'CSS3'
+  },
+  {
+    image: 'SCSS.png',
+    sname: 'SCSS'
+  },
+  {
+    image: 'JS.png',
+    sname: 'JavaScript'
+  },
+  {
+    image: 'JSON.png',
+    sname: 'JSON'
+  },
+  {
+    image: 'ES6.png',
+    sname: 'ES6'
+  },
+  {
+    image: 'WP.png',
+    sname: 'WordPress'
+  },
+  {
+    image: 'BS.png',
+    sname: 'Bootstrap'
+  },
+  {
+    image: 'GIT.png',
+    sname: 'Git'
+  },
+  {
+    image: 'GITHUB.png',
+    sname: 'GitHub'
+  },
+  {
+    image: 'VSC.png',
+    sname: 'VSCode'
+  },
 ]
