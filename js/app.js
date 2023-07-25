a66e183fc03af4f95273a4c06026f84db0e4abbc
Nusrat <nusratilla.88@gmail.com>
2023-07-25 16:43:24 +0500

    link del

--- js/app.js
+++ js/app.js
@@ -1,43 +1,50 @@
-window.addEventListener('DOMContentLoaded', () => {
-    //--------------- Menu Bar ---------------
-    const menuIcon = document.querySelector('#menu-icon');
-    const navbar = document.querySelector('.navbar');
+window.addEventListener("DOMContentLoaded", () => {
+  //--------------- Menu Bar ---------------
+  const menuIcon = document.querySelector("#menu-icon");
+  const navbar = document.querySelector(".navbar");
+  const closeIcon = document.querySelector("#menu-close-icon");
+  const navAnc = document.querySelectorAll(".nav-link");
 
-    menuIcon.addEventListener('click', () => {
-        menuIcon.classList.toggle('bi-x');
-        navbar.classList.toggle('active');
-    });
+  menuIcon.addEventListener("click", () => {
+    navbar.classList.add("active");
+  });
 
-    // ---------------Dark Theme--------------- 
-    
-    const iconMoon = document.querySelector('#icon-moon')
-    const darkMode = localStorage.getItem('darkMode')
+  function linkAction() {
+    navbar.classList.remove("active");
+  }
+  navAnc.forEach((l) => l.addEventListener("click", linkAction));
+  closeIcon.addEventListener("click", linkAction);
 
-    if (darkMode) document.body.classList.add(darkMode)
-    
-    iconMoon.addEventListener('click', () => {
-        document.body.classList.toggle('dark-theme');
-        iconMoon.classList.toggle('bi-sun-fill');
-        if (document.body.classList.contains('dark-theme')) {
-            localStorage.setItem('darkMode', 'dark-theme')
-        } else {
-            localStorage.removeItem('darkMode')
-        }
-    });
+  // ---------------Dark Theme---------------
 
-    // ---------------typed animation--------------- 
-    const typed = new Typed('.name-input', {
-        strings: ['Front-End Web Developer', 'Web Designer'],
-        typeSpeed: 80,
-        backSpeed: 60,
-        loop: true
-    })
+  const iconMoon = document.querySelector("#icon-moon");
+  const darkMode = localStorage.getItem("darkMode");
 
-    // ---------------skills--------------- 
-    let skillHTML = '';
+  if (darkMode) document.body.classList.add(darkMode);
 
-    skillsDb.forEach((skill) => {
-        skillHTML += `
+  iconMoon.addEventListener("click", () => {
+    document.body.classList.toggle("dark-theme");
+    iconMoon.classList.toggle("bi-sun-fill");
+    if (document.body.classList.contains("dark-theme")) {
+      localStorage.setItem("darkMode", "dark-theme");
+    } else {
+      localStorage.removeItem("darkMode");
+    }
+  });
+
+  // ---------------typed animation---------------
+  const typed = new Typed(".name-input", {
+    strings: ["Front-End Web Developer", "Web Designer"],
+    typeSpeed: 80,
+    backSpeed: 60,
+    loop: true,
+  });
+
+  // ---------------skills---------------
+  let skillHTML = "";
+
+  skillsDb.forEach((skill) => {
+    skillHTML += `
         <div class="skill-card">
             <div class="skill-in-card">
               <div class="skill-img">
@@ -49,9 +56,6 @@ window.addEventListener('DOMContentLoaded', () => {
             <div class="skill-anm-bl2"></div>
         </div>
     `;
-    })
-    document.querySelector('.skill-wrapper').innerHTML = skillHTML;
-
-
-
+  });
+  document.querySelector(".skill-wrapper").innerHTML = skillHTML;
 });
