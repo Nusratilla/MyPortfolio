6d08f5a9cdd21653b5c81768581c051d70313aaf
Nusrat <nusratilla.88@gmail.com>
2023-11-12 03:32:00 +0500

    a

--- slider/jsAutoOneCard/app.js
+++ slider/jsAutoOneCard/app.js
@@ -1,16 +1,16 @@
 var swiper = new Swiper(".horizontal-card-big", {
   lazy: true,
-  spaceBetween: 30,
+  spaceBetween: 1,
   loop: true,
   grabCursor: true,
-  // autoplay: {
-  //   delay: 5000,
-  //   disableOnInteraction: false,
-  // },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
-    //   dynamicBullets: true,
+    // dynamicBullets: true,
   },
   navigation: {
     nextEl: ".swiper-button-next",
